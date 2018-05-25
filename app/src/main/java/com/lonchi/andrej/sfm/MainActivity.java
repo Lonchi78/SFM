cc370eb063940f9f8bf18e8d3b7abcf3c872a8fd
Lonchi78 <andrejloncik@gmail.com>
2018-05-25 23:46:40 +0200

    Added Toolbar

--- app/src/main/java/com/lonchi/andrej/sfm/MainActivity.java
+++ app/src/main/java/com/lonchi/andrej/sfm/MainActivity.java
@@ -1,7 +1,11 @@
 package com.lonchi.andrej.sfm;
 
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //  Set Toolbar
+        Toolbar mainToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(mainToolbar);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //  Set menu for toolbar
+        getMenuInflater().inflate(R.menu.overflow_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        //  Handle action buttons clicks
+        switch (item.getItemId()) {
+            case R.id.abRefresh:
+                Toast.makeText(this, "Refresh selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+
+            case R.id.abSettings:
+                Toast.makeText(this, "Settings selected", Toast.LENGTH_SHORT)
+                        .show();
+                break;
+
+            default:
+                break;
+        }
+        return true;
     }
 }
